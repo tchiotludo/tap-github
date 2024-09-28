c81cb3fa82a2aaff89b556697f41177df05090f4
Ludovic DEHON <tchiot.ludo@gmail.com>
2024-09-28 11:55:32 +0200

    Update __init__.py

--- tap_github/__init__.py
+++ tap_github/__init__.py
@@ -196,15 +196,16 @@ def calculate_seconds(epoch):
     return int(round((epoch - current), 0))
 
 def rate_throttling(response):
-    if int(response.headers['X-RateLimit-Remaining']) == 0:
-        seconds_to_sleep = calculate_seconds(int(response.headers['X-RateLimit-Reset']))
-
-        if seconds_to_sleep > MAX_SLEEP_SECONDS:
-            message = "API rate limit exceeded, please try after {} seconds.".format(seconds_to_sleep)
-            raise RateLimitExceeded(message) from None
-
-        logger.info("API rate limit exceeded. Tap will retry the data collection after %s seconds.", seconds_to_sleep)
-        time.sleep(seconds_to_sleep)
+    if 'X-RateLimit-Remaining' in response.headers:
+        if int(response.headers['X-RateLimit-Remaining']) == 0:
+            seconds_to_sleep = calculate_seconds(int(response.headers['X-RateLimit-Reset']))
+    
+            if seconds_to_sleep > MAX_SLEEP_SECONDS:
+                message = "API rate limit exceeded, please try after {} seconds.".format(seconds_to_sleep)
+                raise RateLimitExceeded(message) from None
+    
+            logger.info("API rate limit exceeded. Tap will retry the data collection after %s seconds.", seconds_to_sleep)
+            time.sleep(seconds_to_sleep)
 
 # pylint: disable=dangerous-default-value
 # during 'Timeout' error there is also possibility of 'ConnectionError',
